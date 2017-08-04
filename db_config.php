209eeb8ca12eb68db4bba5315fa9bbe2e568ee93
alen <alen6516@yahoo.com.tw>
2017-08-04 12:24:38 +0800

    add files

---
+++ db_config.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+    global $_DB;    
+    $_DB['servername'] = "localhost";
+    $_DB['username'] = "username";
+    $_DB['password'] = "password";
+    $_DB['dbname'] = "db_name";
+    */
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDB";
+?>
